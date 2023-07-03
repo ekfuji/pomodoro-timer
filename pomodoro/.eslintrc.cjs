270b38fd8f33638264f98e5a8e3fa35e54232170
ekfuji <estefani.fujimoto@hotmail.com>
2023-07-03 15:45:13 -0300

    feat: working and configuring with eslint

--- pomodoro/.eslintrc.cjs
+++ pomodoro/.eslintrc.cjs
@@ -1,12 +1,17 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
+    '@rocketseat/eslint-config/react',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
